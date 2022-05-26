c92ac78d72e64bc456c87af9966d3190dd3c48c6
suraj <soorajkrpandit@gmail.com>
2022-05-26 10:48:01 +0530

    Add project files.

---
+++ AnnotationsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Annotations Demo");
+            AnnotationsTest.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to reflection Demo");
+            CustomerInfo obj = new CustomerInfo();
+            ReflectionOnCustomer.ReflectionTest();
+            obj.Id = -2;
+            Console.WriteLine(obj.Id);
+            Console.ReadKey();
+        }
+    }
+}
